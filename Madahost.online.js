5050a12f5c785dec813bfa3b041e50f42da0b8ad
dimask98 <56661641+dimask98@users.noreply.github.com>
2024-09-25 16:58:03 +0300

    Create Madahost.online.js

---
+++ Madahost.online.js
@@ -0,0 +1,225 @@
+$(document).ready(function() {
+    $('.ae.label.label-primary.fa.fa-bolt, .ae.fa.label.label-primary.fa-microphone').css({
+        'width': '100%',
+        'background-color': '#87CEEB',
+        'margin': '4px 0px 5px 0px',
+        'height': '40px',
+        'border': '1px solid #000',
+        'padding': '0 11px',
+        'font-size': '16px',
+        'display': 'block',
+        'text-align': 'center',
+        'box-sizing': 'border-box',
+        'line-height': '40px'
+    }).text('اضافات');
+
+    $('.ae.label.label-primary.fa.fa-bolt, .ae.fa.label.label-primary.fa-microphone').insertAfter('.setsave');
+});
+
+
+
+$(`<center><div><div width="99.5%" style="background-image:url;color: #160701;border-radius: 5px 5px 5px5px;border-bottom: 2px solid #ffffff;border-top: 2px solid#ffffff;padding-bottom: 2px!important;" >
+<font style="background-color: #fff;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;" color="#003300" > اهلا بكم في شات مـدى للجوال </font></div></div><center>`).insertBefore('#d2');
+$(".label-primary").css("border-radius","31px 30px 30px 31px")
+$('.nav-tabs').addClass('fl').css('width','121%');
+$( "a.label.label-primary.fl > img.fl" ).replaceWith('<img src="" class="fl" style="margin-right: 2px; width: 31px;">');
+$('.nav-tabs').addClass('fl').css('width','100%');
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="">
+<style>
+#as {font-family: 'jazeera-light',FontAwesome;margin-bottom: 2px; color: #160701; border-radius: 5px 5px 5px 5px;
+    text-shadow: 2px 2px 2px rgb(138, 138, 138) , 0px 2px 10px #fff !important;
+border: 1px solid #160701;
+height: 33px;
+text-align: center; 
+}
+.well {font-family: 'jazeera',FontAwesome;font-size: 16px!important;padding: 15px!important;text-align: center!important;width: 101%;color: #003300;white-space: nowrap;border-radius: 14px 14px 14px 14px;}
+.border {shadow:1px 1px 1px 1px ridge rgb(137, 137, 137); box-shadow:1px 1px 1px 1px rgb(137, 137, 137) }
+.fitimg.fl.u-pic  {box-shadow: 0 1px 1px -1px rgb(137, 137, 137), 0 0 5px rgb(137, 137, 137);border-radius: 5px 5px 5px 5px;;}
+ul.nav.nav-tabs.fl li a {      color: #160701;  box-shadow: inset 0 3px 3px rgb(137, 137, 137), 0 0 5px rgb(137, 137, 137);font-family: 'jazeera',FontAwesome;font-size: 12px!important;padding: 5px!important;text-align: center!important;width: 100%;}
+ul.nav.nav-tabs.fl li {border-bottom: 2px solid rgb(138, 137, 137);width: 33.3%;}
+textarea.fl.filw.corner.tbox {    box-shadow:inset 0 0px 0px rgb(137, 137, 137); 0 0 7px rgb(137, 137, 137);border-radius: 7px;padding-left: 5px;}
+label.label.tc.border.btn.fl {border-radius: 41px 21px 41px 21px;}
+.label.tc.border.btn.fl, .label.tc.border.btn.fl {border: 1px solid #003300;font-family: 'jazeera',FontAwesome;border-radius: 4px 4px 4px 4px;}
+label.fl.nosel.label.pnhead {font-family: 'jazeera',FontAwesome;width: 81%;text-align: -webkit-center;background-color: #160701;color: white;border-radius:13px 13px 13px 13px;}
+.fr.borderg a {color: #160701;font-size: 12px!important;}
+.fr.borderg {color: #160701;font-size: 13px!important;border: 1px solid #003300;font-family: 'jazeera',FontAwesome;border-radius: 4px 4px 4px 4px;margin-top: 1px;}
+label.label-primary {border-radius: px 31px 31px 31px;}
+button.btn.fr.btn-success.fa.fa-refresh {
+padding: 4px 5px;
+background-color:"url()"});
+background-image::"url()"});
+border: 1px solid transparent;
+margin-top: -3px;
+border-radius: 50px;
+as:51px;
+}
+.border.corner, .border.corner {box-shadow: inset 0 0px 0px rgb(137, 137, 137), 0 0 4px rgb(137, 137, 137);border: 2px solid #160701;as:13px;}
+.label-primary, .btn-primary, .ae.fa.label.fa-commenting-o.label-warning, .ae.fa.chats.label.fa-comment.label-warning {border: 1px solid #ffffff;border-radius: 31px 31px 31px 31px;as:31px;}
+.checkbox {color: rgb(137, 137, 137);}
+</style>`).insertBefore('body');
+$(`<style> img.fitimg.fl.u-pic { border-radius: 25px!important; box-shadow: inset 0 1px 1px rgb(0 0 0 / 8%), 0 0 8px #707071; } </style>`).insertBefore('body');
+$(`<style> .btn.minix.btn-primary.fa.fa-reply.fr{background:#ffffff00!important;color:#3c763d;border:0;FONT-WEIGHT:500!important}.btn.minix.btn-primary.fa.fa-times.fr{background:#ffffff00!important;color:red;border:0;FONT-WEIGHT:500!important}.btn-danger{background:red!important;border-radius:25px!important}.fl.ustat{height:9px!important;width:9px!important;border-radius:20px 0 0 50px!important}.border,div#tlogins{box-shadow:inset 1px 1px 1px 0 #9494946b,0 0 2px #9494949e!important}.border{border:#e8e8e8 solid 1px}.light{background:repeating-linear-gradient(45deg,rgba(255,255,255,.8),rgb(255,255,255) 100px,#ffffffb0 0,#fff6f699 200px),repeating-linear-gradient(-45deg,rgba(255,255,255,.5),rgba(255,255,255,.93) 100px,#b3adadd1 0,#ffffffe6 200px)!important}.pmsgc{outline:1px dashed #991bf3;outline-offset:-5px;background-size:auto;background-position:100% 0%;background-color:#6694af54!important}.ae.fa.label.label-primary.fa-user{border-left:4px solid #c5c5c5;}
+
+.ae.label.label-primary.fa.fa-bolt,.ae.fa.label.label-primary.fa-microphone{    display:  !important;
+}
+
+ </style>`).insertBefore('body');
+
+
+
+$(document).ready(function() {
+    //  div#mic
+    $('#mic').html('<button id="netflix-btn" style="background-color: black; color: red; border: none; border-radius: 5px; padding: 10px 20px; cursor: pointer; font-size: 16px;">NETFLIX</button>');
+
+    //
+    $('#mic').append('<button id="style1-btn" style="background-color: gray; color: white; border: none; border-radius: 5px; padding: 10px 20px; cursor: pointer; font-size: 16px; margin-left: 10px;">ستايل 🦅</button>');
+
+    //
+    $('#mic').append('<button id="style2-btn" style="background-color: gray; color: white; border: none; border-radius: 5px; padding: 10px 20px; cursor: pointer; font-size: 16px; margin-left: 10px;">ستايل 🌹</button>');
+
+    // 
+    $('#style1-btn').on('click', function() {
+        $.getScript('https://cdn.statically.io/gh/dimask98/nevres.js/main/Style.1.js')
+            .done(function(script, textStatus) {
+                console.log('Style 1 script loaded successfully.');
+            })
+            .fail(function(jqxhr, settings, exception) {
+                console.error('Failed to load Style 1 script:', exception);
+            });
+    });
+
+    //
+    $('#style2-btn').on('click', function() {
+        $.getScript('https://cdn.statically.io/gh/dimask98/nevres.js/main/Style3.js')
+            .done(function(script, textStatus) {
+                console.log('Style 2 script loaded successfully.');
+            })
+            .fail(function(jqxhr, settings, exception) {
+                console.error('Failed to load Style 2 script:', exception);
+            });
+    });
+});
+
+
+
+
+
+
+
+
+</script>
+    <style>
+        /* ◟ M.D.X ◝ */
+        .modal {
+            display: none; /* ◟ M.D.X ◝ */
+            position: fixed;
+            z-index: 1000; /* ◟ M.D.X ◝*/
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.8); /* ◟ M.D.X ◝*/
+        }
+        .modal-content {
+            position: relative;
+            margin: auto;
+            padding: 0;
+            width: 80%;
+            height: 80%;
+            background-color: white;
+            border-radius: 5px;
+            transition: all 0.3s; /* ◟ M.D.X ◝*/
+        }
+        .modal-header {
+            background-color: black;
+            color: white;
+            padding: 10px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+        .modal-body {
+            padding: 10px;
+            height: calc(100% - 50px); /* ◟ M.D.X ◝*/
+        }
+        .close, .maximize, .minimize {
+            cursor: pointer;
+            color: white;
+            margin-left: 10px;
+            background-color: black;
+            border: none;
+            padding: 5px 10px;
+            border-radius: 5px;
+        }
+        .close:hover, .maximize:hover, .minimize:hover {
+            background-color: rgba(255, 0, 0, 0.7); /* ◟ M.D.X ◝ */
+        }
+        iframe {
+            width: 100%;
+            height: 100%;
+            border: none;
+        }
+    </style>
+</head>
+<body>
+
+<div id="mic" style="height: 100%; width: 100%;" class="break light tab-pane border active">
+    <!--  NETFLIX -->
+    <button id="netflix-btn" style="background-color: black; color: red; border: none; border-radius: 5px; padding: 10px 20px; cursor: pointer; font-size: 16px;">
+        NETFLIX
+    </button>
+</div>
+
+<!-- ◟ M.D.X ◝ -->
+<div class="modal" id="netflixModal">
+    <div class="modal-content" id="modalContent">
+        <div class="modal-header">
+            <h5>NETFLIX</h5>
+            <div>
+                <button class="minimize" id="minimizeModal">تصغير</button>
+                <button class="maximize" id="maximizeModal">تكبير</button>
+                <button class="close" id="closeModal">إغلاق</button>
+            </div>
+        </div>
+        <div class="modal-body">
+            <iframe src="https://web2.topcinema.cam/"></iframe>
+        </div>
+    </div>
+</div>
+
+<script>
+$(document).ready(function() {
+    // 
+    $('#netflix-btn').on('click', function() {
+        $('#netflixModal').fadeIn(); // 
+    });
+
+    // 
+    $('#closeModal').on('click', function() {
+        $('#netflixModal').fadeOut(); // 
+    });
+
+    // 
+    $(window).on('click', function(event) {
+        if ($(event.target).is('#netflixModal')) {
+            $('#netflixModal').fadeOut(); // 
+        }
+    });
+
+    // 
+    $('#minimizeModal').on('click', function() {
+        $('#modalContent').css({
+            width: '40%', // 
+            height: '40%' // 
+        });
+    });
+
+    // 
+    $('#maximizeModal').on('click', function() {
+        $('#modalContent').css({
+            width: '100%', // 
+            height: '100%' // 
+        });
+    });
+});
